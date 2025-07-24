21f35461a00f17d3ddaebb69c63fbeef3f6b401b
jessu@8003 <pochajaswanthr@gmail.com>
2025-07-24 12:27:46 +0530

    Delete products from cart when clicking delete

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,4 +1,4 @@
-import {cart} from '../data/cart.js';
+import {cart,removeFromCart} from '../data/cart.js';
 import {products} from '../data/products.js';
 import { formatCurrency} from './utils/money.js';
 let cartSummaryHTML=''
@@ -35,7 +35,7 @@ cart.forEach((cartItem)=>{
             <span class="update-quantity-link link-primary">
             Update
             </span>
-            <span class="delete-quantity-link link-primary">
+            <span class="delete-quantity-link link-primary js-delete-link" data-product-id=${matchingProduct.id}>
             Delete
             </span>
         </div>
@@ -48,7 +48,7 @@ cart.forEach((cartItem)=>{
         <div class="delivery-option">
             <input type="radio" checked
             class="delivery-option-input"
-            name="delivery-option-${matchingProduct}">
+            name="delivery-option-${matchingProduct.id}">
             <div>
             <div class="delivery-option-date">
                 Tuesday, June 21
@@ -61,7 +61,7 @@ cart.forEach((cartItem)=>{
         <div class="delivery-option">
             <input type="radio"
             class="delivery-option-input"
-            name="delivery-option-${matchingProduct}">
+            name="delivery-option-${matchingProduct.id}">
             <div>
             <div class="delivery-option-date">
                 Wednesday, June 15
@@ -74,7 +74,7 @@ cart.forEach((cartItem)=>{
         <div class="delivery-option">
             <input type="radio"
             class="delivery-option-input"
-            name="delivery-option-${matchingProduct}">
+            name="delivery-option-${matchingProduct.id}">
             <div>
             <div class="delivery-option-date">
                 Monday, June 13
@@ -90,3 +90,11 @@ cart.forEach((cartItem)=>{
     `
 })
 document.querySelector('.js-order-summary').innerHTML=cartSummaryHTML;
+document.querySelectorAll('.js-delete-link')
+.forEach((link)=>{
+    link.addEventListener('click',()=>{
+        const productId=link.dataset.productId;
+        removeFromCart(productId);
+        console.log(cart)
+    });
+});
