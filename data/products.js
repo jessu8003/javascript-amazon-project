d1a7280c125ce49c44aa364c40aa5d3d05b82fc9
jessu@8003 <pochajaswanthr@gmail.com>
2025-07-31 11:34:03 +0530

    Use Inheritance and Polymorphism for sizechart

--- data/products.js
+++ data/products.js
@@ -30,10 +30,26 @@ class Product {
     return `$${formatCurrency(this.priceCents)}`;
 
   }
+  extraInfoHTML(){
+    return ' ';
+  }
 
 }
 
+class Clothing extends Product{
+  sizeChartLink;
+  constructor(productDetails){
+    super(productDetails);
+    this.sizeChartLink=productDetails.sizeChartLink;
 
+  }
+  extraInfoHTML(){
+    return `
+    <a href="${this.sizeChartLink}" target="_blank">Size Chart</a>`;
+  }
+
+
+}
 export const products = [
   {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
@@ -694,6 +710,10 @@ export const products = [
     ]
   }
 ].map((productDetails)=>{
+  if(productDetails.type==='clothing'){
+    return new Clothing(productDetails);
+
+  }
   return new Product(productDetails);
 
 });
