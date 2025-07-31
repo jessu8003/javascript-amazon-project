9da3c0cc5b9fbdbdc093241eac0c1827ca5fa8dc
jessu@8003 <pochajaswanthr@gmail.com>
2025-07-31 16:02:04 +0530

    Learn more details about classes and this

--- data/products.js
+++ data/products.js
@@ -47,9 +47,30 @@ class Clothing extends Product{
     return `
     <a href="${this.sizeChartLink}" target="_blank">Size Chart</a>`;
   }
-
-
 }
+/*const date=new Date();
+console.log(date.toLocaleString());
+console.log(date);
+
+console.log(this);
+const object2={
+  a:2,
+  b:this.a
+}
+  
+ function logThis(){
+  console.log(this)
+ }
+ logThis();
+ logThis.call('hello');
+this
+ const pbject3={
+  method:()=>{
+    console.log(this);
+  }
+ };
+ pbject3.method();
+ */
 export const products = [
   {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
